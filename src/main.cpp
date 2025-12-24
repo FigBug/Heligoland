a95093255041d83c9e6922795672a2047b60bcb2
Roland Rabien <figbug@gmail.com>
2025-12-23 23:05:37 -0800

    Fix build

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include "Game.h"
 
+#if defined(_WIN32)
+#include <windows.h>
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    (void) hInstance;
+    (void) hPrevInstance;
+    (void) lpCmdLine;
+    (void) nCmdShow;
+
+    Game game;
+
+    if (! game.init())
+    {
+        return 1;
+    }
+
+    game.run();
+    game.shutdown();
+
+    return 0;
+}
+
+#else
+
 int main (int argc, char* argv[])
 {
     (void) argc;
@@ -17,3 +42,5 @@ int main (int argc, char* argv[])
 
     return 0;
 }
+
+#endif
